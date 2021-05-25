570dd303c1d09ae2ef78a31f1fd9831b38f91c25
iExploze <54289660+iExploze@users.noreply.github.com>
2021-05-25 12:59:22 -0400

    added draw method to states

--- src/States.java
+++ src/States.java
@@ -1,4 +1,10 @@
+import java.awt.*;
+
 public abstract class States {
     //FIXME make the class
     //TODO make and change all the screen to States
+    public void draw(Graphics g)
+    {
+
+    }
 }
