7ed88dbe1df75f0d03a9b441b0d6d8a8d6eb43a0
iExploze <54289660+iExploze@users.noreply.github.com>
2021-06-01 22:03:51 -0400

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Ball {
+    //test
     //instance variable
     private int x;    //top left x value
     private int y;    //top left y value
